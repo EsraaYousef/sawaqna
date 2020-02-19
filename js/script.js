565c5017d120c05263e6479098d393174acc624f
EsraaYousef <esraayousef400@gmail.com>
2020-02-19 21:58:34 +0200

    edit button disabled

--- js/script.js
+++ js/script.js
@@ -185,27 +185,6 @@ function previewFile(input,image) {
         });
     }, false);
   })();
-  var toValidate = jQuery("input"),
-  valid = false;
-toValidate.keyup(function () {
-  if (jQuery(this).val().length > 0) {
-      jQuery(this).data("valid", true);
-  } else {
-      jQuery(this).data("valid", false);
-  }
-  toValidate.each(function () {
-      if (jQuery(this).data("valid") == true) {
-          valid = true;
-      } else {
-          valid = false;
-      }
-  });
-  if (valid === true) {
-      jQuery("button[type=submit]").prop("disabled", false);
-  } else {
-      jQuery("button[type=submit]").prop("disabled", true);
-  }
-});
 
 
 // loading window
